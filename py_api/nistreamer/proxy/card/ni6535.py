bfc98d043defdbfd98900a1d876042e8b19b0324
ps-cd <45522676+ps-cd@users.noreply.github.com>
2025-01-24 18:02:08 -0500

    [Bug fix] Forgot to update PyAPI proxies

--- py_api/nistreamer/proxy/card/ni6535.py
+++ py_api/nistreamer/proxy/card/ni6535.py
@@ -4,13 +4,21 @@ from nistreamer.channel import DOChanProxy
 
 class NI6535(DOCardProxy):
     def __repr__(self):
-        return 'DO card NI6535 ' + BaseCardProxy.__repr__(self)
+        return super().__repr__() + '\n\n\tCard model: NI6535'
 
-    def add_chan(self, chan_idx: int, default_value: bool = False, nickname: str = None, proxy_class=DOChanProxy):
+    def add_chan(
+            self,
+            chan_idx: int,
+            dflt_val: bool = False,
+            rst_val: bool = False,
+            nickname: str = None,
+            proxy_class=DOChanProxy
+    ):
         return super().add_chan(
             port_idx=chan_idx // 8,
             line_idx=chan_idx % 8,
-            default_value=default_value,
+            dflt_val=dflt_val,
+            rst_val=rst_val,
             nickname=nickname,
             proxy_class=proxy_class
         )
