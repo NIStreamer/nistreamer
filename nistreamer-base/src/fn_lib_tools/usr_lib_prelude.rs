676aede3d30e1171f5180fd54e275de1e0cb818b
ps-cd <45522676+ps-cd@users.noreply.github.com>
2025-08-11 18:26:14 -0400

    Renamed `nistreamer-macros`

--- nistreamer-base/src/fn_lib_tools/usr_lib_prelude.rs
+++ nistreamer-base/src/fn_lib_tools/usr_lib_prelude.rs
@@ -1,4 +1,4 @@
 pub use pyo3::prelude::*;
 
 pub use crate::fn_lib_tools::{Calc, FnBoxF64, FnBoxBool};
-pub use nistreamer_fn_lib_macros::{usrlib_boilerplate, usr_fn_f64, usr_fn_bool};
+pub use nistreamer_macros::{usrlib_boilerplate, usr_fn_f64, usr_fn_bool};
